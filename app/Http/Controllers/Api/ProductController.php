d6038defbaf8880ff7ad177298bbed5babefb18a
danielpsrb <danielpsrb01@gmail.com>
2024-07-26 20:32:52 +0700

    product controller

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    // Get all products
+    public function index(Request $request)
+    {
+        $products = Product::where('seller_id', $request->user()->id)->with('seller')->get();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Products',
+            'data' => $products,
+        ]);
+    }
+
+    // Store product
+    public function store(Request $request)
+    {
+        $request->validate([
+            'category_id' => 'required|exists:categories,id',
+            'name' => 'required|string',
+            'description' => 'string',
+            'price' => 'required',
+            'stock' => 'required|integer',
+            'image' => 'required|image|mimes:jpg,jpeg,png|max:2048',
+        ]);
+
+        $image = null;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('assets/product', 'public');
+        }
+
+        $product = Product::create([
+            'seller_id' => $request->user()->id,
+            'category_id' => $request->category_id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock,
+            'image' => $image,
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Product created',
+            'data' => $product,
+        ], 201);
+    }
+
+    // Update product
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'category_id' => 'required|exists:categories,id',
+            'name' => 'required|string',
+            'description' => 'string',
+            'price' => 'required',
+            'stock' => 'required|integer',
+            'image' => 'image|mimes:jpg,jpeg,png|max:2048',
+        ]);
+
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Product not found',
+            ], 404);
+        }
+
+        $product->update([
+            'category_id' => $request->category_id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock,
+        ]);
+
+        if ($request->hasFile('image')) {
+            //Delete old image if exists
+            if($product->image) {
+                Storage::delete('public/products/' . $product->image);
+            }
+
+            $image_path = $request->file('image');
+            $image_name = $image_path->hashName();
+            $image_path->storeAs('public/products', $image_name);
+            $product->image = $image_name;
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Product updated',
+            'data' => $product,
+        ]);
+    }
+
+    // Delete product
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Product not found',
+            ], 404);
+        }
+
+        $product->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Product deleted',
+        ]. 200);
+    }
+}
